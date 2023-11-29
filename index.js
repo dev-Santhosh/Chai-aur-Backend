b4ecfb4d677b324ebec29f13e73ebe11c204a9b5
Santhosh <spinisetti6@gmail.com>
2023-11-29 16:44:05 +0530

    commit first backend basic application

---
+++ index.js
@@ -0,0 +1,17 @@
+// console.log("Chai aur Code");
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!!');
+});
+
+app.get('/login',(req,res)=>{
+  res.send("<h1>Login to see the page</h1>")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
